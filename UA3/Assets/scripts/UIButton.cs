18c06b1510800baeca3f2260395b4f4d7f578ba3
Jesus <jflorespadilla@nevada.unr.edu>
2018-11-24 03:13:49 -0800

    Finnished memory game in one go

---
+++ UA3/Assets/scripts/UIButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class UIButton : MonoBehaviour {
+    [SerializeField] private GameObject targetObject;
+    [SerializeField] private string targetMessage;
+    private Color highlightColor = Color.cyan;
+
+    public void OnMouseEnter() {
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+
+        if (sprite != null) {
+            sprite.color = highlightColor;
+        }
+    }
+    public void OnMouseExit() {
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if (sprite != null) {
+            sprite.color = Color.white;
+        }
+    }
+
+    public void OnMouseDown() {
+        transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
+    }
+
+    public void OnMouseUp() {
+        transform.localScale = Vector3.one;
+        if (targetObject != null) {
+            targetObject.SendMessage(targetMessage);
+        }
+    }
+}
